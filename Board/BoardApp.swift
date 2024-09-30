87c5aeb1e4aa4b08b3834fc7d4a7759596493f15
Youngmin322 <choyoungmin0322@gmail.com>
2024-09-30 14:01:35 +0900

    Initial Commit

---
+++ Board/BoardApp.swift
@@ -0,0 +1,34 @@
+//
+//  BoardApp.swift
+//  Board
+//
+//  Created by 조영민 on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BoardApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
